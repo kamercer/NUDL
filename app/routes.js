26a1eae059e427d4356443e19cc68b3ba4ab36f9
Kyle <kyle.mercer.km@gmail.com>
2015-10-23 19:58:33 -0400

    setup

---
+++ app/routes.js
@@ -0,0 +1,30 @@
+// app/routes.js
+
+//var Nerd = require('./models/nerd');
+
+module.exports = function(app){
+    //server routes
+
+    //sample route
+    /*
+    app.get('/api/nerds', function(req, res){
+	
+	//user mongoose to get all nerds in the database
+	Nerd.find(function(err, nerds){
+	    
+	    if (err){
+		res.send(err);
+	    }
+	    
+	    res.json(nerds);
+		
+	});
+    });
+    */
+
+    //frontend routes
+    //route to handle all angular requests
+    app.get('*', function(req, res){
+	res.sendfile('./public/views/index.html');
+    });
+}
